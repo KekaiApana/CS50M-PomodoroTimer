58cb96904683e633fd6429ebb3a84fa39872fb27
Kekai Apana <kekaiapana@gmail.com>
2021-11-05 20:05:22 -1000

    Changed export to default

--- components/styles.js
+++ components/styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
@@ -23,3 +23,5 @@ export const styles = StyleSheet.create({
         flexDirection: 'row',
     }
 });
+
+export default styles
